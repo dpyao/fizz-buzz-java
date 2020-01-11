35681873c03232f3c43f656be76684095a71033e
dpyao <anlihuer@outlook.com>
2020-01-11 23:07:21 +0700

    return expected string if number can be divided by 3,5 or 7

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,6 +1,10 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
+    public Boolean isNumberCanDivided (int number, int divisor) {
+        return number % divisor == 0;
+    }
+
     public String getOutputString(int number) {
         String numberString = Integer.toString(number);
 
@@ -10,6 +14,19 @@ public class FizzBuzz {
             return "BuzzWhizz";
         if(numberString.contains("3"))
             return "Fizz";
+
+        if(this.isNumberCanDivided(number, 3) || this.isNumberCanDivided(number, 5) || this.isNumberCanDivided(number, 7)) {
+            String result = "";
+
+            if(this.isNumberCanDivided(number, 3))
+                result += "Fizz";
+            if(this.isNumberCanDivided(number, 5))
+                result += "Buzz";
+            if(this.isNumberCanDivided(number, 7))
+                result += "Whizz";
+
+            return result;
+        }
 
         return numberString;
     }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -1,10 +1,6 @@
 package com.thoughtworks;
 
 import org.junit.Test;
-import org.junit.runners.Parameterized;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,6 +19,8 @@ public class FizzBuzzTest {
         String actualFive = fizzBuzz.getOutputString(13);
         String actualSix = fizzBuzz.getOutputString(37);
         String actualSeven = fizzBuzz.getOutputString(75);
+        String actualEight = fizzBuzz.getOutputString(27);
+        String actualNine = fizzBuzz.getOutputString(30);
 
         //then
         assertEquals(actualOne,"Fizz");
@@ -32,6 +30,8 @@ public class FizzBuzzTest {
         assertEquals(actualFive,"Fizz");
         assertEquals(actualSix,"Fizz");
         assertEquals(actualSeven,"Fizz");
+        assertEquals(actualEight,"Fizz");
+        assertEquals(actualNine,"Fizz");
     }
 
     @Test
@@ -44,11 +44,29 @@ public class FizzBuzzTest {
         String actualTwo = fizzBuzz.getOutputString(25);
         String actualThree = fizzBuzz.getOutputString(50);
         String actualFour = fizzBuzz.getOutputString(35);
+        String actualFive = fizzBuzz.getOutputString(15);
 
         //then
         assertEquals(actualOne,"BuzzWhizz");
         assertEquals(actualTwo,"BuzzWhizz");
         assertEquals(actualThree,"BuzzWhizz");
         assertEquals(actualFour,"BuzzWhizz");
+        assertEquals(actualFive,"BuzzWhizz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_number_can_be_divided_by_3() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String actualOne = fizzBuzz.getOutputString(9);
+        String actualTwo = fizzBuzz.getOutputString(21);
+        String actualThree = fizzBuzz.getOutputString(210);
+
+        //then
+        assertEquals(actualOne,"Fizz");
+        assertEquals(actualTwo,"FizzWhizz");
+        assertEquals(actualThree,"FizzBuzzWhizz");
     }
 }
